82b05199f1eb1341f1d4c8ef8175d438bc66942f
ImranGreet <hossainimran1208@gmail.com>
2025-12-23 07:49:44 +0600

    refactor abbrevationCreator function; add type annotations and implement formatTime24To12 and formatDate functions

--- src/store/helper.ts
+++ src/store/helper.ts
@@ -1,4 +1,4 @@
-let abbrevationCreator = (fullText: string) => {
+let abbrevationCreator = (fullText: string):string => {
 	let words = fullText.split(' ');
 	let abbrevation = words
 		.map((word) => word.charAt(0).toUpperCase())
@@ -6,4 +6,30 @@ let abbrevationCreator = (fullText: string) => {
 	return abbrevation;
 };
 
-export { abbrevationCreator };
+let formatTime24To12 = (time: string): string => {
+	if (!time) return '';
+	const value: string = time.replace(':', '.');
+	let [hoursStr, minutesStr = '00'] = value.split('.');
+
+	const hours: number = Number(hoursStr);
+	const minutes: number = Number(minutesStr);
+
+	if (Number.isNaN(hours) || Number.isNaN(minutes)) return '';
+
+	const period: 'AM' | 'PM' = hours >= 12 ? 'PM' : 'AM';
+	const formattedHours: number = hours % 12 || 12;
+
+	return `${formattedHours}:${minutes.toString().padStart(2, '0')} ${period}`;
+};
+let formatDate = (dateString: string): string => {
+	let date = new Date(dateString);
+	let options: Intl.DateTimeFormatOptions = {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+	};
+
+	return date.toLocaleDateString(undefined, options);
+};
+
+export { abbrevationCreator, formatDate, formatTime24To12 };
